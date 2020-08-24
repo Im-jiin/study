351c3b385f682949e17771cf60faab6c44bc51db
jiin <ji___in_96@naver.com>
2020-08-24 16:21:32 +0900

    REST API

---
+++ src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,28 @@
+package com.example.study.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+    @RequestMapping(method = RequestMethod.GET, path = "/getMethod") //localhost :8080/api/getmethod
+    public  String getRequest (){
+        return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter") //locatlhost:8080/api/getparameter?id=123&password=abcd
+    public String getParameter(@RequestParam String id,@RequestParam String password)
+    {
+        System.out.println("id : " +id);
+        System.out.println(("password : " +password));
+
+        return id+password;
+    }
+
+   // public String getMeltiParameter(@RequestParam String account,
+   //                                 @RequestParam String email,
+   //                                 @RequestParam int Page){
+//
+ //   }
+
+}
