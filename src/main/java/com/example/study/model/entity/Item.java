9428b1582dfe25ab1b1c560f964610a95d5f46e6
jiin <ji___in_96@naver.com>
2020-08-24 17:40:45 +0900

    Entity 설정

---
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -0,0 +1,31 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;  // 등록 / 해지 / 검수중(등록대기중)
+    private String name;
+    private String title;
+    private String content;
+    private BigDecimal price;
+    private String brandName;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
